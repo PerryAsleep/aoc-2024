e1968326a0a2ebc19cce1abfbb9e7719c40bd6b0
Perry Lynch <perryklynch@gmail.com>
2024-12-22 13:45:18 -0500

    day 21

---
+++ day-21/Part1.cs
@@ -0,0 +1,430 @@
+﻿using System.Text;
+
+internal class Part1
+{
+	private const string File = "input.txt";
+
+	public const int L = 0;
+	public const int D = 1;
+	public const int U = 2;
+	public const int R = 3;
+	public const int NumDirs = 4;
+	public const int NumRobotsOnDPads = 2;
+
+	public static readonly List<Tuple<int, int>> MoveDirs = new()
+	{
+		new Tuple<int, int>(-1, 0), // L
+		new Tuple<int, int>(0, 1),  // D
+		new Tuple<int, int>(0, -1), // U
+		new Tuple<int, int>(1, 0),  // R
+	};
+
+	internal abstract class KeyPad
+	{
+		public abstract KeyPadState CreateInitialKeyPadState();
+		protected abstract bool IsValidPos(Tuple<int, int> pos);
+
+		protected List<List<Tuple<int, int>>> GetPossibleSequencesToMoveTo(Tuple<int, int> from, Tuple<int, int> to)
+		{
+			var result = new List<List<Tuple<int, int>>>();
+
+			// No change in position.
+			if (from.Item1 == to.Item1 && from.Item2 == to.Item2)
+				return null;
+
+			// Only change in X.
+			if (from.Item2 == to.Item2)
+			{
+				result.Add(GetXPath(from.Item1, to.Item1));
+				return result;
+			}
+
+			// Only change in Y.
+			if (from.Item1 == to.Item1)
+			{
+				result.Add(GetYPath(from.Item2, to.Item2));
+				return result;
+			}
+
+			// Change in both X and Y. Try X then Y, and then Y then X.
+			var xPath = GetXPath(from.Item1, to.Item1);
+			var yPath = GetYPath(from.Item2, to.Item2);
+
+			var xThenY = new List<Tuple<int, int>>();
+			xThenY.AddRange(xPath);
+			xThenY.AddRange(yPath);
+			if (IsPathValid(from, xThenY))
+				result.Add(xThenY);
+
+			var yThenX = new List<Tuple<int, int>>();
+			yThenX.AddRange(yPath);
+			yThenX.AddRange(xPath);
+			if (IsPathValid(from, yThenX))
+				result.Add(yThenX);
+
+			return result;
+		}
+
+		protected List<List<Tuple<int, int>>> AddButtonPressToSequences(List<List<Tuple<int, int>>> sequences)
+		{
+			// There are existing sequences. Each one needs a button press move added to it.
+			if (sequences != null)
+			{
+				foreach (var sequence in sequences)
+				{
+					sequence.Add(null);
+				}
+				return sequences;
+			}
+
+			// No existing sequences. We still need to one which has one move to press the button.
+			Tuple<int, int> buttonPress = null;
+			var movesToButtonPress = new List<Tuple<int, int>>() { buttonPress };
+			sequences = new List<List<Tuple<int, int>>>() { movesToButtonPress };
+			return sequences;
+		}
+
+		private bool IsPathValid(Tuple<int, int> from, List<Tuple<int, int>> path)
+		{
+			var pos = from;
+			foreach (var move in path)
+			{
+				pos = new Tuple<int, int>(pos.Item1 + move.Item1, pos.Item2 + move.Item2);
+				if (!IsValidPos(pos))
+					return false;
+			}
+			return true;
+		}
+
+		private static List<Tuple<int, int>> GetXPath(int x1, int x2)
+		{
+			var path = new List<Tuple<int, int>>();
+			while (x1 < x2)
+			{
+				path.Add(new Tuple<int, int>(1, 0));
+				x1++;
+			}
+
+			while (x1 > x2)
+			{
+				path.Add(new Tuple<int, int>(-1, 0));
+				x1--;
+			}
+			return path;
+		}
+
+		private static List<Tuple<int, int>> GetYPath(int y1, int y2)
+		{
+			var path = new List<Tuple<int, int>>();
+			while (y1 < y2)
+			{
+				path.Add(new Tuple<int, int>(0, 1));
+				y1++;
+			}
+
+			while (y1 > y2)
+			{
+				path.Add(new Tuple<int, int>(0, -1));
+				y1--;
+			}
+			return path;
+		}
+
+		protected static int GetCostToMove(Tuple<int, int> from, Tuple<int, int> to)
+		{
+			return Math.Abs(to.Item1 - from.Item1) + Math.Abs(to.Item2 - from.Item2);
+		}
+	}
+
+	/// <summary>
+	///     +---+---+
+	///     | ^ | A |
+	/// +---+---+---+
+	/// | < | v | > |
+	/// +---+---+---+
+	/// </summary>
+	internal class DPad : KeyPad
+	{
+		public static readonly List<Tuple<int, int>> PosDirs = new()
+		{
+			new Tuple<int, int>(0, 1), // L
+			new Tuple<int, int>(1, 1), // D
+			new Tuple<int, int>(1, 0), // U
+			new Tuple<int, int>(2, 1), // R
+		};
+		public static readonly Tuple<int, int> PosA = new(2, 0);
+
+		public override KeyPadState CreateInitialKeyPadState()
+		{
+			return new KeyPadState(PosA);
+		}
+
+		public static (KeyPadState, int) GetCostToMove(KeyPadState state, Tuple<int, int> move)
+		{
+			if (move == null)
+				return (new KeyPadState(PosA), GetCostToMove(state.Pos, PosA) + 1);
+			for (var d = 0; d < NumDirs; d++)
+				if (Equals(move, MoveDirs[d]))
+					return (new KeyPadState(PosDirs[d]), GetCostToMove(state.Pos, PosDirs[d]) + 1);
+			return (null, int.MaxValue);
+		}
+
+		protected override bool IsValidPos(Tuple<int, int> pos)
+		{
+			return pos.Item1 >= 0 && pos.Item1 <= 2 && pos.Item2 >= 0 && pos.Item2 <= 1 && !(pos.Item1 == 0 && pos.Item2 == 0);
+		}
+
+		public (KeyPadState, List<List<Tuple<int, int>>>) GetPossibleSequencesToEnterMoveOnNextKeyPad(KeyPadState state, Tuple<int, int> moveOnNextKeyPad)
+		{
+			var (newState, sequences) = moveOnNextKeyPad == null ? GetPossibleSequencesToMoveToA(state) : GetPossibleSequencesToMoveToDir(state, moveOnNextKeyPad);
+			sequences = AddButtonPressToSequences(sequences);
+			return (newState, sequences);
+		}
+
+		private (KeyPadState, List<List<Tuple<int, int>>>) GetPossibleSequencesToMoveToDir(KeyPadState state, Tuple<int, int> moveOnNextKeyPad)
+		{
+			for (var d = 0; d < NumDirs; d++)
+				if (Equals(moveOnNextKeyPad, MoveDirs[d]))
+					return GetPossibleSequencesToMoveToDir(state, d);
+			return (null, null);
+		}
+
+		private (KeyPadState, List<List<Tuple<int, int>>>) GetPossibleSequencesToMoveToDir(KeyPadState state, int d)
+		{
+			return (new KeyPadState(PosDirs[d]), GetPossibleSequencesToMoveTo(state.Pos, PosDirs[d]));
+		}
+
+		private (KeyPadState, List<List<Tuple<int, int>>>) GetPossibleSequencesToMoveToA(KeyPadState state)
+		{
+			return (new KeyPadState(PosA), GetPossibleSequencesToMoveTo(state.Pos, PosA));
+		}
+	}
+
+	/// <summary>
+	/// +---+---+---+
+	/// | 7 | 8 | 9 |
+	/// +---+---+---+
+	/// | 4 | 5 | 6 |
+	/// +---+---+---+
+	/// | 1 | 2 | 3 |
+	/// +---+---+---+
+	///     | 0 | A |
+	///     +---+---+
+	/// </summary>
+	internal class PinPad : KeyPad
+	{
+		public static readonly List<Tuple<int, int>> PosNums = new()
+		{
+			new Tuple<int, int>(1, 3),
+			new Tuple<int, int>(0, 2),
+			new Tuple<int, int>(1, 2),
+			new Tuple<int, int>(2, 2),
+			new Tuple<int, int>(0, 1),
+			new Tuple<int, int>(1, 1),
+			new Tuple<int, int>(2, 1),
+			new Tuple<int, int>(0, 0),
+			new Tuple<int, int>(1, 0),
+			new Tuple<int, int>(2, 0),
+		};
+		public static readonly Tuple<int, int> PosA = new(2, 3);
+
+		public override KeyPadState CreateInitialKeyPadState()
+		{
+			return new KeyPadState(PosA);
+		}
+
+		protected override bool IsValidPos(Tuple<int, int> pos)
+		{
+			return pos.Item1 >= 0 && pos.Item1 <= 2 && pos.Item2 >= 0 && pos.Item2 <= 3 && !(pos.Item1 == 0 && pos.Item2 == 3);
+		}
+
+		public (KeyPadState, List<List<Tuple<int, int>>>) GetPossibleSequencesToEnterNum(KeyPadState state, int num)
+		{
+			return (new KeyPadState(PosNums[num]), AddButtonPressToSequences(GetPossibleSequencesToMoveTo(state.Pos, PosNums[num])));
+		}
+
+		public (KeyPadState, List<List<Tuple<int, int>>>) GetPossibleSequencesToEnterA(KeyPadState state)
+		{
+			return (new KeyPadState(PosA), AddButtonPressToSequences(GetPossibleSequencesToMoveTo(state.Pos, PosA)));
+		}
+	}
+
+	internal class KeyPadState
+	{
+		public readonly Tuple<int, int> Pos;
+		public KeyPadState(Tuple<int, int> inPos)
+		{
+			Pos = inPos;
+		}
+	}
+
+	private readonly List<string> Codes = new();
+
+	public void Run()
+	{
+		ParseInput();
+
+		var result = 0;
+		foreach (var code in Codes)
+		{
+			var sequence = GetShortestSequence(code);
+			PrintSequence(code, sequence);
+			result += sequence.Count * int.Parse(code.Substring(0, code.Length - 1));
+		}
+		Console.WriteLine(result);
+	}
+
+	private void ParseInput()
+	{
+		var sr = new StreamReader(File);
+		var line = sr.ReadLine();
+		while (!string.IsNullOrEmpty(line))
+		{
+			Codes.Add(line);
+			line = sr.ReadLine();
+		}
+	}
+
+	private List<Tuple<int, int>> GetShortestSequence(string code)
+	{
+		var possibleSequences = GetPossibleSequencesForPinPad(code);
+		var sequence = ChooseBestSequence(possibleSequences);
+		for (var dpadIndex = 0; dpadIndex < NumRobotsOnDPads; dpadIndex++)
+		{
+			possibleSequences = GetPossibleSequencesForDPad(sequence);
+			sequence = ChooseBestSequence(possibleSequences);
+		}
+		return sequence;
+	}
+
+	private List<List<Tuple<int, int>>> GetPossibleSequencesForPinPad(string code)
+	{
+		var pinPad = new PinPad();
+		var pinPadState = pinPad.CreateInitialKeyPadState();
+		var possibleSequences = new List<List<Tuple<int, int>>>();
+
+		var codeIndex = 0;
+		var state = pinPadState;
+		while (codeIndex< code.Length)
+		{
+			var pinPadKey = code[codeIndex];
+
+			List<List<Tuple<int, int>>> sequencesToNextPinPadKey = null;
+			KeyPadState newState;
+			if (int.TryParse(pinPadKey.ToString(), out var pinPadNum))
+				(newState, sequencesToNextPinPadKey) = pinPad.GetPossibleSequencesToEnterNum(state, pinPadNum);
+			else
+				(newState, sequencesToNextPinPadKey) = pinPad.GetPossibleSequencesToEnterA(state);
+
+			if (possibleSequences.Count > 0)
+			{
+				var newSequences = new List<List<Tuple<int, int>>>();
+				foreach (var possibleSequence in possibleSequences)
+				{
+					foreach (var nextSequence in sequencesToNextPinPadKey)
+					{
+						var combined = new List<Tuple<int, int>>();
+						combined.AddRange(possibleSequence);
+						combined.AddRange(nextSequence);
+						newSequences.Add(combined);
+					}
+				}
+				possibleSequences = newSequences;
+			}
+			else
+			{
+				possibleSequences = sequencesToNextPinPadKey;
+			}
+
+			state = newState;
+			codeIndex++;
+		}
+
+		return possibleSequences;
+	}
+
+	private List<List<Tuple<int, int>>> GetPossibleSequencesForDPad(List<Tuple<int, int>> sequence)
+	{
+		var dPad = new DPad();
+		var state = dPad.CreateInitialKeyPadState();
+		var possibleSequences = new List<List<Tuple<int, int>>>();
+
+		var i = 0;
+		foreach (var move in sequence)
+		{
+			var (newState, sequencesToNext) = dPad.GetPossibleSequencesToEnterMoveOnNextKeyPad(state, move);
+
+			if (possibleSequences.Count > 0)
+			{
+				var newSequences = new List<List<Tuple<int, int>>>();
+				foreach (var possibleSequence in possibleSequences)
+				{
+					foreach (var nextSequence in sequencesToNext)
+					{
+						var combined = new List<Tuple<int, int>>();
+						combined.AddRange(possibleSequence);
+						combined.AddRange(nextSequence);
+						newSequences.Add(combined);
+					}
+				}
+				possibleSequences = newSequences;
+			}
+			else
+			{
+				possibleSequences = sequencesToNext;
+			}
+			state = newState;
+			i++;
+		}
+
+		return possibleSequences;
+	}
+
+	private List<Tuple<int, int>> ChooseBestSequence(List<List<Tuple<int, int>>> possibleSequences)
+	{
+		var dPad = new DPad();
+
+		// Choose best sequence.
+		var bestCost = int.MaxValue;
+		var bestIndex = 0;
+		for (var sequenceIndex = 0; sequenceIndex < possibleSequences.Count; sequenceIndex++)
+		{
+			var sequence = possibleSequences[sequenceIndex];
+			var dPadState = dPad.CreateInitialKeyPadState();
+			var totalCost = 0;
+			foreach (var move in sequence)
+			{
+				(dPadState, var cost) = DPad.GetCostToMove(dPadState, move);
+				totalCost += cost;
+			}
+
+			if (totalCost < bestCost)
+			{
+				bestCost = totalCost;
+				bestIndex = sequenceIndex;
+			}
+		}
+
+		return possibleSequences[bestIndex];
+	}
+
+	private void PrintSequence(string code, List<Tuple<int, int>> sequence)
+	{
+		var sb = new StringBuilder();
+		sb.Append($"{code}: ");
+		foreach (var move in sequence)
+		{
+			if (move == null)
+				sb.Append("A");
+			else if (move.Equals(MoveDirs[U]))
+				sb.Append("U");
+			else if (move.Equals(MoveDirs[D]))
+				sb.Append("D");
+			else if (move.Equals(MoveDirs[L]))
+				sb.Append("L");
+			else if (move.Equals(MoveDirs[R]))
+				sb.Append("R");
+		}
+		Console.WriteLine(sb);
+	}
+}
---
+++ day-21/Part2.cs
@@ -0,0 +1,144 @@
+﻿internal class Part2
+{
+	private const string File = "input.txt";
+
+	private const int NumPinPadRobots = 25;
+	private const int NumRobots = NumPinPadRobots + 1;
+	private Dictionary<string, long>[] Caches = new Dictionary<string, long>[NumRobots];
+	private readonly List<string> Codes = new();
+
+	/// <summary>
+	/// DPad:
+	///    +---+---+
+	///    | U | A |
+	///+---+---+---+
+	///| L | D | R |
+	///+---+---+---+
+	///
+	/// PinPad:
+	/// +---+---+---+
+	/// | 7 | 8 | 9 |
+	/// +---+---+---+
+	/// | 4 | 5 | 6 |
+	/// +---+---+---+
+	/// | 1 | 2 | 3 |
+	/// +---+---+---+
+	///     | 0 | A |
+	///     +---+---+
+	/// 
+	/// For each move from one button to another, the possible sequences of moves on the next
+	/// pad in the chain which needs to be input to generate that move. These paths will never
+	/// change directions more than once as that would not produce an optimal path on the next pad.
+	/// </summary>
+	private static readonly Dictionary<string, List<string>> PadMoves = new()
+	{
+		// Pinpad moves. Only the ones we need.
+		{"A1", new() { "ULLA" }},
+		{"14", new() { "UA" }},
+		{"40", new() { "RDDA" }},
+		{"0A", new() { "RA" }},
+		{"17", new() { "UUA" }},
+		{"70", new() { "RDDDA" }},
+		{"16", new() { "RRUA", "URRA" }},
+		{"69", new() { "UA" }},
+		{"9A", new() { "DDDA" }},
+		{"A8", new() { "LUUUA", "UUULA" }},
+		{"80", new() { "DDDA" }},
+		{"03", new() { "RUA", "URA" }},
+		{"3A", new() { "DA" }},
+		{"12", new() { "RA" }},
+		{"29", new() { "RUUA", "UURA" }},
+
+		// DPad moves.
+		{"AA", new() { "A" }},
+		{"AL", new() { "DLLA" }},
+		{"AD", new() { "DLA", "LDA" }},
+		{"AU", new() { "LA" }},
+		{"AR", new() { "DA" }},
+		{"LA", new() { "RRUA" }},
+		{"LL", new() { "A" }},
+		{"LD", new() { "RA" }},
+		{"LU", new() { "RUA" }},
+		{"LR", new() { "RRA" }},
+		{"RA", new() { "UA" }},
+		{"RL", new() { "LLA" }},
+		{"RD", new() { "LA" }},
+		{"RU", new() { "LUA", "ULA" }},
+		{"RR", new() { "A" }},
+		{"UA", new() { "RA" }},
+		{"UL", new() { "DLA" }},
+		{"UD", new() { "DA" }},
+		{"UU", new() { "A" }},
+		{"UR", new() { "DRA", "RDA" }},
+		{"DA", new() { "RUA", "URA" }},
+		{"DL", new() { "LA" }},
+		{"DD", new() { "A" }},
+		{"DU", new() { "UA" }},
+		{"DR", new() { "RA" }},
+	};
+
+	public void Run()
+	{
+		ParseInput();
+
+		var result = 0L;
+		foreach (var code in Codes)
+		{
+			// Clear cache.
+			for (var r = 0; r < NumRobots; r++)
+				Caches[r] = new Dictionary<string, long>();
+
+			// Prepend A since the robots start at A and need to move from it.
+			var codeMoves = "A" + code;
+
+			// Start looping at 1 to account for the prepended A.
+			var total = 0L;
+			for (var i = 1; i < codeMoves.Length; i++)
+				total += GetCount(codeMoves.Substring(i - 1, 2), NumRobots - 1);
+			result += total * int.Parse(code.Substring(0, code.Length - 1));
+		}
+		Console.WriteLine(result);
+	}
+
+	private void ParseInput()
+	{
+		var sr = new StreamReader(File);
+		var line = sr.ReadLine();
+		while (!string.IsNullOrEmpty(line))
+		{
+			Codes.Add(line);
+			line = sr.ReadLine();
+		}
+	}
+
+	private long GetCount(string instruction, int r)
+	{
+		// Get the moves required to execute the instruction.
+		var moves = PadMoves[instruction];
+
+		// The final robot. Even with the potential for multiples moves to execute
+		// the instruction they will all be the same length.
+		if (r == 0)
+			return moves[0].Length;
+
+		// Check each possible move to execute the instruction.
+		var bestCount = long.MaxValue;
+		foreach (var move in moves)
+		{
+			// Prepend an A.
+			// If this is very first move, it must start on A.
+			// For all other moves, A must have been pressed last in order to make the next robot press its button.
+			var fullMove = "A" + move;
+
+			if (!Caches[r].TryGetValue(fullMove, out var count))
+			{
+				// Start at 1 to account for the prepended A.
+				for (var i = 1; i < fullMove.Length; i++)
+					count += GetCount(fullMove.Substring(i - 1, 2), r - 1);
+				Caches[r][fullMove] = count;
+			}
+			bestCount = Math.Min(count, bestCount);
+		}
+		return bestCount;
+	}
+}
