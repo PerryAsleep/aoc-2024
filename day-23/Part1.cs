7818109fc60bfd7fcba51f104d8b37d88de87072
Perry Lynch <perryklynch@gmail.com>
2024-12-23 09:06:05 -0500

    day 23

---
+++ day-23/Part1.cs
@@ -0,0 +1,93 @@
+﻿internal class Part1
+{
+	private const string File = "input.txt";
+
+	private class Node
+	{
+		public Node(string id)
+		{
+			Id = id;
+		}
+
+		public readonly string Id;
+		public readonly Dictionary<string, Node> Neighbors = new();
+	}
+
+	private readonly Dictionary<string, Node> AllNodes = new();
+
+	public void Run()
+	{
+		ParseInput();
+
+		var sets = GetAllSetsOfThree();
+
+		var count = 0;
+		foreach (var set in sets)
+		{
+			foreach (var setKvp in set)
+			{
+				if (setKvp.Key.StartsWith('t'))
+				{
+					count++;
+					break;
+				}
+			}
+		}
+
+		Console.WriteLine(count);
+	}
+
+	private void ParseInput()
+	{
+		var sr = new StreamReader(File);
+		var line = sr.ReadLine();
+		while (!string.IsNullOrEmpty(line))
+		{
+			var parts = line.Split("-");
+			AllNodes.TryAdd(parts[0], new Node(parts[0]));
+			AllNodes.TryAdd(parts[1], new Node(parts[1]));
+			AllNodes[parts[0]].Neighbors.TryAdd(parts[1], AllNodes[parts[1]]);
+			AllNodes[parts[1]].Neighbors.TryAdd(parts[0], AllNodes[parts[0]]);
+			line = sr.ReadLine();
+		}
+	}
+
+	private List<Dictionary<string, Node>> GetAllSetsOfThree()
+	{
+		HashSet<int> setIds = new();
+		List<Dictionary<string, Node>> sets = new();
+
+		foreach (var aKvp in AllNodes)
+		{
+			foreach (var bKvp in aKvp.Value.Neighbors)
+			{
+				foreach (var cKvp in bKvp.Value.Neighbors)
+				{
+					if (cKvp.Value.Neighbors.ContainsKey(aKvp.Key))
+					{
+						var setHash = HashIds(aKvp.Key, bKvp.Key, cKvp.Key);
+						if (!setIds.Contains(setHash))
+						{
+							setIds.Add(setHash);
+							sets.Add(new Dictionary<string, Node>
+							{
+								[aKvp.Key] = aKvp.Value,
+								[bKvp.Key] = bKvp.Value,
+								[cKvp.Key] = cKvp.Value,
+							});
+						}
+					}
+				}
+			}
+		}
+
+		return sets;
+	}
+
+	private int HashIds(string a, string b, string c)
+	{
+		var l = new List<string>() { a, b, c };
+		l.Sort();
+		return HashCode.Combine(l[0], l[1], l[2]);
+	}
+}
---
+++ day-23/Part2.cs
@@ -0,0 +1,95 @@
+﻿using System.Text;
+
+internal class Part2
+{
+	private const string File = "input.txt";
+
+	private class Node
+	{
+		public Node(string id)
+		{
+			Id = id;
+		}
+
+		public readonly string Id;
+		public readonly Dictionary<string, Node> Neighbors = new();
+	}
+
+	private readonly Dictionary<string, Node> AllNodes = new();
+
+	public void Run()
+	{
+		ParseInput();
+		WriteClique(GetMaximumClique());
+	}
+
+	private void WriteClique(Dictionary<string, Node> clique)
+	{
+		// Make list of sorted ids.
+		var idList = new List<string>();
+		foreach (var kvp in clique)
+			idList.Add(kvp.Key);
+		idList.Sort();
+
+		// Write formatted output.
+		var sb = new StringBuilder();
+		var first = true;
+		foreach (var id in idList)
+		{
+			if (!first)
+				sb.Append(',');
+			sb.Append(id);
+			first = false;
+		}
+
+		Console.WriteLine(sb);
+	}
+
+	private void ParseInput()
+	{
+		var sr = new StreamReader(File);
+		var line = sr.ReadLine();
+		while (!string.IsNullOrEmpty(line))
+		{
+			var parts = line.Split("-");
+			AllNodes.TryAdd(parts[0], new Node(parts[0]));
+			AllNodes.TryAdd(parts[1], new Node(parts[1]));
+			AllNodes[parts[0]].Neighbors.TryAdd(parts[1], AllNodes[parts[1]]);
+			AllNodes[parts[1]].Neighbors.TryAdd(parts[0], AllNodes[parts[0]]);
+			line = sr.ReadLine();
+		}
+	}
+
+	private Dictionary<string, Node> GetMaximumClique()
+	{
+		var largestClique = new Dictionary<string, Node>();
+		foreach (var n in AllNodes)
+		{
+			var clique = GetMaximumClique(n.Value);
+			if (clique.Count > largestClique.Count)
+				largestClique = clique;
+		}
+
+		return largestClique;
+	}
+
+	private Dictionary<string, Node> GetMaximumClique(Node r)
+	{
+		var largestClique = new Dictionary<string, Node>
+		{
+			{ r.Id, r },
+		};
+		foreach (var n in AllNodes)
+			if (IsAdjacentToAllNodes(n.Value, largestClique))
+				largestClique.Add(n.Key, n.Value);
+		return largestClique;
+	}
+
+	private bool IsAdjacentToAllNodes(Node n, Dictionary<string, Node> otherNodes)
+	{
+		foreach (var otherNode in otherNodes)
+			if (!n.Neighbors.Contains(otherNode))
+				return false;
+		return true;
+	}
+}
